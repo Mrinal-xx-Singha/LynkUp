9f9fea416bc541d4f1c5f6e348972682ba9fd569
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2025-04-03 13:21:31 +0530

    db modelling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,24 @@
 import Link from "next/link";
 
-import MobileNavbar from "@/components/MobileNavbar"
-import DesktopNavbar from "@/components/DesktopNavbar"
+import MobileNavbar from "@/components/MobileNavbar";
+import DesktopNavbar from "@/components/DesktopNavbar";
 const Navbar = () => {
   return (
     <nav className="sticky top-0 border-b w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <Link
-                href="/"
-                className="text-xl font-bold text-primary font-mono tracking-wider"
-              >
-                LinkUp
-              </Link>
-            </div>
-
-            <DesktopNavbar />
-            {/* <MobileNavbar /> */}
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className="text-xl font-bold text-primary font-mono tracking-wider"
+            >
+              LinkUp
+            </Link>
           </div>
-          </div>
- 
+          <DesktopNavbar />
+          <MobileNavbar />
+        </div>
+      </div>
     </nav>
   );
 };
